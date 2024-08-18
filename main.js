a43788fd794e71ab3d66156fd056124f6bb638cd
Trammell <e_g_1117@hotmail.com>
2024-08-18 15:30:04 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+let name = prompt("Please enter your name: ");
+let number1 = prompt('Please enter a number between 1 and 10: ');
+let number2 = prompt('Please enter a number between 11 and 20: ');
+
+let userName = prompt("Please enter your name: ");
+let var1 = +prompt("Please enter the first number: ");
+let var2 = +prompt("Please enter the second number: ");
+
+console.log("var1:", var1);
+console.log("var2", var2);
+
+let userResults = var1 * var2;
+console.log("The result of multiplying var1 and var2 is: ", userResults);
+
+
+
